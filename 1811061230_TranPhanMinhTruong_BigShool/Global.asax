ed2c35a7e5e23fb010371786d10638c027ea88d6
Truongbaby <truongtran819@gmail.com>
2021-06-30 13:56:06 +0700

    Add project files.

---
+++ 1811061230_TranPhanMinhTruong_BigShool/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_1811061230_TranPhanMinhTruong_BigShool.MvcApplication" Language="C#" %>
---
+++ 1811061230_TranPhanMinhTruong_BigShool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_1811061230_TranPhanMinhTruong_BigShool.Startup))]
+namespace _1811061230_TranPhanMinhTruong_BigShool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
